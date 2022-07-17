4e7c1f35c40187a6ce3478f2f383775c5a71e484
alan.r <reale.ag@gmail.com>
2022-07-17 19:35:39 -0300

    arreglando html

--- js/main.js
+++ js/main.js
@@ -16,36 +16,36 @@ boton.onclick=()=>{
     document.getElementById("precioFinal").value = total;
 }; */
 // PROYECTO FINAL 2
-
+/* 
 const productos = [
-    {id:1, nombre: "Casaca Boca años '60", descripción: "", 
+    {id:1, nombre: "Casaca Boca años '60", descripción: "Camiseta Original de Boca Juniors década de los sesenta", 
     imagen:"./img/boca60.jpg", cat: 1, precio: 7499},
 
-    {id:2, nombre: "Casaca Argentina Titular '86", descripción: "", 
+    {id:2, nombre: "Casaca Argentina Titular '86", descripción: "Camiseta icónica de la Selección Argentina Copa Mundial de México 1986", 
     imagen:"./img/argentina86.jpg", cat: 1, precio: 6500},
 
-    {id:3, nombre: "Casaca Holanda Titular '74", descripción: "",
+    {id:3, nombre: "Casaca Holanda Titular '74", descripción: "La Naranja Mecánica, clásica camiseta de Holanda del 74",
     imagen:"./img/holanda74.jpg", cat: 1, precio: 6500},
 
-    {id:4, nombre: "Casaca Brasil Titular '82", descripción: "", 
+    {id:4, nombre: "Casaca Brasil Titular '82", descripción: "Hermosa camiseta de la Selección de Brasil Copa Mundial de España 1982", 
     imagen:"./img/brasil82.jpg", cat: 1, precio: 4150},
 
-    {id:5, nombre: "Casaca Argentina Suplente '86", descripción: "", 
+    {id:5, nombre: "Casaca Argentina Suplente '86", descripción: "La Barrilete Cósmico, no requiere presentación. Suplente Selección Argentina 1986", 
     imagen:"./img/argentinaSup86.jpg", cat: 1, precio: 7500},
 
-    {id:6, nombre: "Casaca Alemania Titular '74", descripción: "", 
+    {id:6, nombre: "Casaca Alemania Titular '74", descripción: "Armadura utilizada por el artillero Gerd Müller en 1974", 
     imagen:"./img/alemania74.jpg", cat: 1, precio: 5000},
 
-    {id:7, nombre: "Casaca Italia Titular '70", descripción: "", 
+    {id:7, nombre: "Casaca Italia Titular '70", descripción: "Hermosa camiseta de La Nazionale década de los setenta", 
     imagen:"./img/italia70.jpg", cat: 1, precio: 5750},
 
-    {id:8, nombre: "Pelota Tango '78", descripción: "", 
+    {id:8, nombre: "Pelota Tango '78", descripción: "Icónica pelota Adidas Tango de la Copa Mundial de Argentina 1978", 
     imagen:"./img/pelotaTango78.jpg", cat: 2, precio: 2150},
 
-    {id:9, nombre: "Pelota Tricolore '98", descripción: "", 
+    {id:9, nombre: "Pelota Tricolore '98", descripción: "La Tricolore utilizada en la Copa Mundial de Francia 1998", 
     imagen:"./img/pelotaTricolore98.jpg", cat: 2, precio: 2000},
 
-    {id:10, nombre: "Pelota Azteca '86", descripción: "", 
+    {id:10, nombre: "Pelota Azteca '86", descripción: "Pelota Azteca, protagonista del Mejor Gol de la Historia. Copa Mundial México 1986", 
     imagen:"./img/pelotaAzteca86.jpg", cat: 2, precio: 3999},
 ];
 
@@ -60,6 +60,14 @@ function saveProdLocal(productos) {
 function searchProd(id) {
     let productos = loadProdLocal();
     return productos.find(x => x.id ==id);
+} */
+class Info {
+    constructor (nombre, email, direccion){
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+    }
 }
+function datosCliente() {
 
-
+}
